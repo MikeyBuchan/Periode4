2481c7fe19e062a1824778225b1343d0cd92103b
MatthijsRisch <matthijs_r555@hotmail.com>
2018-06-18 15:22:13 +0200

    item add script

---
+++ P4Zombie/Assets/Scripts/PlayerController/WeaponAddScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponAddScript : MonoBehaviour {
+    public PlayerController playerScript;
+    public GameObject weaponGameobject;
+
+    public void Weapon()
+    {
+        Debug.Log("IT DOES SOMETHING");
+        playerScript.Weapons.Add(weaponGameobject);
+    }
+}
