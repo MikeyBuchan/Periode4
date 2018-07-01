ec3278447d5bf962c3b2ef7ce7765b8e3156e2d0
MatthijsRisch <matthijs_r555@hotmail.com>
2018-07-01 23:58:54 +0200

    idk

---
+++ P4Zombie/Assets/Scripts/Weapons/BulletsAmount.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BulletsAmount : MonoBehaviour {
+    public GameObject weapon;
+    public GameObject player;
+    public int ammo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        weapon = player.GetComponent<PlayerController>().Weapons[0];
+        ammo = weapon.GetComponent<DesserdEagle>().ammo;
+        gameObject.GetComponent<Text>().text = "" + weapon.GetComponent<DesserdEagle>().ammo;
+	}
+}
