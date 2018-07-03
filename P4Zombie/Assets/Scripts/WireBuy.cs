5fcb133372a28cd838dbacb8ee20f77cfdc47f14
Mas Tijben <Whosobvious@gmail.com>
2018-07-03 02:26:34 +0200

    wire

---
+++ P4Zombie/Assets/Scripts/WireBuy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireBuy : MonoBehaviour
+{
+    void BuyWire()
+    {
+        GameObject.FindGameObjectWithTag("WireTag").SetActive(true);
+        GameObject.FindGameObjectWithTag("Player")
+    }
+}
